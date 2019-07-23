2c02a4e03a4e803761e4ef986d94f76064a0000b
TitanSoftwareDevelopment <atlas@titansoftwaredevelopment.com>
2019-07-23 03:12:50 -0700

    Reset To Rebeccas site

--- javascript/pageChange.js
+++
@@ -1,9 +0,0 @@
-function mainPage() {
-    document.getElementById('main-page').style.backgroundColor = '#EA4343'
-    document.getElementById('community-page').style.backgroundColor = '#2D2D2D'
-}
-
-function communityPage() {
-    document.getElementById('main-page').style.backgroundColor = '#2D2D2D'
-    document.getElementById('community-page').style.backgroundColor = '#EA4343'
-}
